7675788f0dda4f6a80ff735d058cbf3f4021a6fc
programacaotonhub <evertonmoura1975@gmail.com>
2026-03-09 00:25:14 +0000

    alterando o arquivo comecando.c

--- comecando.c
+++ comecando.c
@@ -1,1 +1,34 @@
+#includ <studio.h>
 
+int main() {
+    /*
+    Soma (+)
+    Subtação (-)
+    Multiplicação (*)
+    Divisão (/)
+    */
+
+    int numero1, numero2;
+    int soma, subtrtacao, multiplicacao, divisao;
+
+    printf("Digite o primeiro número: \n");
+    scanf("%d", &numero1);
+    printf("Digite o segundo número: \n");
+    scanf("%d", &numero2);
+
+    //operação soma
+    soma = numero1 + numero2;
+    //operação subtração
+    subtracao = numero1 - numero2;
+    //operação multiplicação
+    multiplicacao = numero1 * numero2;
+    //operação divisão
+    divisao = numero1 / numero2;
+
+    printf("A soma dos números é: %d\n", soma);
+    printf("A subtração dos números é: %d\n", subtracao);
+    printf("A multiplicação dos números é: %d\n", multiplicacao);
+    printf("A divisão dos números é: %d\n", divisao);
+    
+    return 0;
+}
