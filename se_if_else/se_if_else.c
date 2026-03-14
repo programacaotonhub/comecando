0eb54a6119178590da87b1c0254a25a79d839e9f
programacaotonhub <evertonmoura1975@gmail.com>
2026-03-14 23:02:16 +0000

    remove arquivo comecando.c e adiciona novo arquivo se_if_else.c com comparação de números

---
+++ se_if_else/se_if_else.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+int numero1, numero2;
+
+number1 = 10;
+number2 = 5;
+
+if(numero1 > numero2) {
+    printf("O número 1 é maior que o número 2.\n");
+} else if(numero1 < numero2) {
+    printf("O número 1 é menor que o número 2.\n");
+} else {
+    printf("O número 1 é igual ao número 2.\n");
+}
